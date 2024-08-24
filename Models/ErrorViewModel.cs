b1aa47b4aae8484422cda7e0df137d3c9335873c
RobinsonHL18 <herreral.jrobinson@gmail.com>
2024-08-24 18:53:28 -0500

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PC1_HerreraLopez.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
